5b22377e3b094ebb904b0e4d4d23d25659a36c30
Rafael Parola <rafaelparola@host.dynamic.voo.be>
2025-11-23 10:29:56 +0100

    Removing unnecessary code.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {MainPage} from "./pages/MainPage.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
         <MainPage></MainPage>
